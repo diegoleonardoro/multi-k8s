1d973140d51a119d576a934091e846e200cc3f67
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2023-01-17 09:47:41 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hola bebe, como estas?
+            hola, mi amor, como eastas?
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
